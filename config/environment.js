143268f086da344279b71630ee4d30f3f79121cf
Steve Witman <stevewitman@gmail.com>
2015-02-03 10:57:36 -0700

    added csp, adapter, posts resource

--- config/environment.js
+++ config/environment.js
@@ -12,7 +12,15 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       }
     },
-
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' http://localhost:3000",
+      'img-src': "'self'",
+      'style-src': "'self' 'unsafe-inline'",
+      'media-src': "'self'"
+    },
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
